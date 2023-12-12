a0334cf1cee3d0cd193def1aed771ca33c8513df
kubsnn <51138889+kubsnn@users.noreply.github.com>
2023-12-12 01:49:54 +0100

    + updated api describtion

--- client/src/api.py
+++ client/src/api.py
@@ -24,6 +24,7 @@ invite()            | Sends an invitation request to the server to invite a user
 accept()            | Sends an accept request to the server to accept an invitation from a user with the provided username.
 invitations()       | Sends a request to the server to get a list of pending invitations from other users.
 friends()           | Sends a request to the server to get a list of friends.
+remove()            | Sends a request to the server to remove a friend.
                     |
 send()              | Sends a message to the specified receiver.
 """
